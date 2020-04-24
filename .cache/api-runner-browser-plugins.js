6fec75d8510449a1dd0e8a435318bc3f5c986e35
Natalie Famula <nataliefamula@Natalie.local>
2020-04-24 00:16:27 -0400

    fixing things

--- .cache/api-runner-browser-plugins.js
+++
@@ -1,7 +0,0 @@
-module.exports = [{
-      plugin: require('../node_modules/gatsby-remark-images/gatsby-browser.js'),
-      options: {"plugins":[],"maxWidth":2048},
-    },{
-      plugin: require('../node_modules/gatsby-plugin-netlify-cms/gatsby-browser.js'),
-      options: {"plugins":[],"modulePath":"/Users/nataliefamula/Desktop/mediation-website/src/cms/cms.js"},
-    }]
